61b5f4023d52dfdfa725ca72a36005e84e56828f
Daniel3dartist <danielsobrinho3dartist@gmail.com>
2024-07-15 22:49:34 -0300

    added files

---
+++ app/src/main/java/com/example/hex_sheet/character_sheet.kt
@@ -0,0 +1,20 @@
+package com.example.hex_sheet
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class character_sheet : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_character_sheet)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.character_sheet)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
